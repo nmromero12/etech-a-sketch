e63e7e2cdd18e2abc72ec22e402bfbe424747749
Nicholas Romero <nicholas_romero@brown.edu>
2025-03-08 18:05:04 -0500

    columns added to grid

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector(".grid-container");
 
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    container.appendChild(div);
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
 }
 
 const rows = document.querySelectorAll('.row');
+
+rows.forEach(row => {
+   for (let i = 0; i < 16; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    row.appendChild(column);
+   }
+
+});
 
